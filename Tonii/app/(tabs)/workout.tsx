1c7650ce19bebbc55538a38d8f5063f9e8986562
Ayush Tamang <tamangayush052@gmail.com>
2025-02-18 22:14:08 +0545

    added pages with basic styling

---
+++ Tonii/app/(tabs)/workout.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+
+const workout = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Workout</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5', // Light gray background
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333', // Dark gray text
+  },
+});
+
+export default workout
